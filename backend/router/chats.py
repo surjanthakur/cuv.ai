00f3410552049bb8dc2eedfc9409e53d3571b812
surjanthakur <tsurjan506@gmail.com>
2025-10-17 19:06:35 +0530

    save

--- backend/router/chats.py
+++ backend/router/chats.py
@@ -1,1 +1,9 @@
 from fastapi import APIRouter, HTTPException, status
+
+
+router = APIRouter(tags=["Chats"])
+
+
+@router.post("/chats", status_code=status.HTTP_201_CREATED)
+async def create_chat():
+    pass
