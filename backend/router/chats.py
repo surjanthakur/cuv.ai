9947ea6022e9346d0860f455bd69e256c5f57bed
surjanthakur <tsurjan506@gmail.com>
2025-10-18 20:25:57 +0530

    save

--- backend/router/chats.py
+++ backend/router/chats.py
@@ -7,3 +7,8 @@ router = APIRouter(tags=["Chats"])
 @router.post("/chats", status_code=status.HTTP_201_CREATED)
 async def create_chat():
     pass
+
+
+@router.post("/chats/{id}/messages", status_code=status.HTTP_201_CREATED)
+async def get_message():
+    pass
