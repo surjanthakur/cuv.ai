3b2663c813da9bd5e278111c9448a1e260c03902
surjanthakur <tsurjan506@gmail.com>
2025-10-30 08:50:12 +0530

    save

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,28 +1,10 @@
+import { useState, useEffect } from 'react';
 import './sidebar.css';
-import { Sidebar, Plus, MessageSquare } from 'lucide-react';
 
 export default function SidebarWindow() {
   return (
     <>
-      <section className="sidebar">
-        <Sidebar className="w-5 h-5 text-white" />
-        <span>Cuv.ai</span>
-        <div>
-          <Plus className="w-5 h-5 text-white" />
-          <button>New Chat</button>
-        </div>
-        <MessageSquare />
-        <span>Chats</span>
-        <div className="recentchats">
-          <ul>
-            <li>chats</li>
-            <li>chats</li>
-            <li>chats</li>
-            <li>chats</li>
-            <li>chats</li>
-          </ul>
-        </div>
-      </section>
+      <section></section>
     </>
   );
 }
